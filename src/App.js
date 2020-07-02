4d45929a770e3c5f036048762c9410c86a547ac3
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-07-02 14:32:27 +0100

    Naviagation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Styles/main.css";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Nav />
+      <div className="landing-page" id="intro">
+        <h1 className="name">Ric</h1>
+        <svg
+          className="h-letter"
+          width="460"
+          height="555"
+          viewBox="0 0 460 555"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Learn React
-        </a>
-      </header>
+          <path
+            d="M335.15 0.599976H459.95V555H335.15V319.8H125.55V555H0.75V0.599976H125.55V212.6H335.15V0.599976Z"
+            fill="#7A9E7E"
+          />
+        </svg>
+
+        <h1 className="name">ard</h1>
+      </div>
+      <section className="test" id="about"></section>
     </div>
   );
 }
---
+++ src/Components/Nav.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+
+function Nav() {
+  const [isActive, toggleActive] = useState(false);
+  const [isTop, setIsTop] = useState(true);
+
+  const handleClick = () => {
+    toggleActive(!isActive);
+  };
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const scrollCheck = window.scrollY < 300;
+      console.log("Scroll Check");
+      if (scrollCheck !== isTop) {
+        setIsTop(!isTop);
+      }
+    });
+  });
+
+  return (
+    <nav className={isTop ? "nav" : "nav scrolled"}>
+      <div class="flex-wrapper">
+        <a className="nav__logo" href="#intro">
+          Richard Coric
+        </a>
+        <div
+          class={isActive ? "hamburger open" : "hamburger"}
+          onClick={handleClick}
+        >
+          <div className="burger"></div>
+        </div>
+      </div>
+      <ul className={isActive ? "nav__list active" : "nav__list"}>
+        <li className="nav__link">
+          <a href="#about" onClick={handleClick}>
+            About
+          </a>
+        </li>
+        <li className="nav__link">
+          <a href="#projects" onClick={handleClick}>
+            Projects
+          </a>
+        </li>
+        <li className="nav__link">
+          <a href="#blog" onClick={handleClick}>
+            Blog
+          </a>
+        </li>
+        <li className="nav__link">
+          <a href="#contact" onClick={handleClick}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
